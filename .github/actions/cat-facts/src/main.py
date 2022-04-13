ed48cf805490b3a17ad5a6f2e8b39ef118da1392
eve <30925984+eve000017@users.noreply.github.com>
2022-04-13 09:39:44 +0800

    Update main.py

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -13,12 +13,12 @@ fact_list = []
 
 # Add the "text" of every object into the fact_list list
 for fact in r_obj_list:
-  fact_list.append(fact["text"])
-  
+    fact_list.append(fact["text"])
+
 # Select a random fact from the fact_list and return it
 # into a variable named random_fact so we can use it
 def select_random_fact(fact_arr):
-  return fact_arr[random.randint(0, len(fact_list)+1)]
+    return fact_arr[random.randint(0, len(fact_list)+1)]
 
 random_fact = select_random_fact(fact_list)
 
